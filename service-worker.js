b3c5075de3f61a24b00be0ab791d9016579393b4
ayesha54 <akaleem306@gmail.com>
2022-01-09 18:52:01 +0000

    Deploying to gh-pages from  @ b8d13af972d7c962599f25b89d125c0a13d9da0a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bc636e583fd052962f452974acfd5d7.js"
+  "/developerFolio/precache-manifest.d2cce67a6b5402b014aa76b5725f7712.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
